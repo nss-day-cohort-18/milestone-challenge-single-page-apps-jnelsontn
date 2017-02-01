abe507327c8c1deb9356b5b2efeb500b98c18ccb
Jordan Nelson <jnelsontn@gmail.com>
2017-02-01 15:38:05 -0600

    almost there

--- javascript/eventhandlers.js
+++ javascript/eventhandlers.js
@@ -1,13 +1,7 @@
 
 var CarLot = (function (original) {
 
-	original.changeBorder = function(domElement, color){
-		domElement.classList.add(color);
- 	};
 
- 	original.resetBorder = function(domElement, color){
- 		domElement.classList.remove(color);
- 	};
 
 return original;
 
